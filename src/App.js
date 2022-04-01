3ccaaf0f74ab334484a7172f1ad15084dbdb7a7a
Offireland <irlanda_barnett@unikino.edu.mx>
2022-04-01 00:57:16 -0700

    Ya quedo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Cuadro1, {Cuadro2} from "./componentes/Cuadro1";
+import Saludo, {Bye, bye} from "./componentes/Saludo";
+
+function App() {
+  return (
+    <>
+    <Bye/>
+    <Saludo/>
+    <Cuadro1/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componentes/Saludo.js
@@ -0,0 +1,11 @@
+export default function Saludo(){
+    return(<h1>
+        Hi
+    </h1>);
+}
+
+export function Bye(){
+    return(<h1>
+        Adios
+    </h1>);
+}
